9d64bd478407d1d3f91021833bae70e888434906
liptondev <zam.kuratora@gmail.com>
2017-07-03 20:28:52 +0300

    Added new audio format

--- SpeechKitCloud/ASR/Enums.cs
+++ SpeechKitCloud/ASR/Enums.cs
@@ -13,24 +13,44 @@
         public readonly string format;
 
         /// <summary>
-        /// Медиаконтейнер - WAV.
+        /// Медиаконтейнер WAV может содержать аудио данные любого формата, например PCM.
         /// </summary>
         public static AudioFormat WAV = new AudioFormat("audio/x-wav");
 
         /// <summary>
-        /// Медиаконтейнер - MP3.
+        /// MPEG-1 Audio Layer 3 (MP3).
         /// </summary>
         public static AudioFormat MP3 = new AudioFormat("audio/x-mpeg-3");
 
         /// <summary>
-        /// Медиаконтейнер - OGG (.ogg, .spx).
+        /// Speex.
         /// </summary>
         public static AudioFormat OGG = new AudioFormat("audio/x-speex");
 
         /// <summary>
-        /// Медиаконтейнер - OGG (.ogg, .opus).
+        /// Opus.
         /// </summary>
         public static AudioFormat OGGOpus = new AudioFormat("audio/ogg;codecs=opus");
+
+        /// <summary>
+        /// Opus.
+        /// </summary>
+        public static AudioFormat WebM = new AudioFormat("audio/webm;codecs=opus");
+
+        /// <summary>
+        /// Linear PCM c частотой дискретизации 16000 Гц и разрядностью квантования 16 бит.
+        /// </summary>
+        public static AudioFormat PCM16Bit16kRate = new AudioFormat("audio/x-pcm;bit=16;rate=16000");
+
+        /// <summary>
+        /// Linear PCM c частотой дискретизации 8000 Гц и разрядностью квантования 16 бит.
+        /// </summary>
+        public static AudioFormat PCM16Bit8kRate = new AudioFormat("audio/x-pcm;bit=16;rate=8000");
+
+        /// <summary>
+        /// PCM на основе A-law. Частота дискретизации 8000 Гц, разрядность квантования 13 бит.
+        /// </summary>
+        public static AudioFormat PCM13Bit8kRate = new AudioFormat("audio/x-alaw;bit=13;rate=8000");
     }
 
     /// <summary>
