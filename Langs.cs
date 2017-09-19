f9c6b05abeef254d82f3fce5f158cc0da93ff434
liptondev <zam.kuratora@gmail.com>
2017-09-19 17:58:11 +0300

    new version

---
+++ Langs.cs
@@ -0,0 +1,35 @@
+﻿namespace YandexAPI
+{
+    /// <summary>
+    /// Язык.
+    /// </summary>
+    public sealed class Langs
+    {
+        private Langs(string lang) => Lang = lang;
+
+        /// <summary>
+        /// Выбранный язык.
+        /// </summary>
+        public string Lang { get; }
+
+        /// <summary>
+        /// Русский язык.
+        /// </summary>
+        public static Langs Ru = new Langs("ru-RU");
+
+        /// <summary>
+        /// Английский язык.
+        /// </summary>
+        public static Langs En = new Langs("en-US");
+
+        /// <summary>
+        /// Украинский язык.
+        /// </summary>
+        public static Langs Uk = new Langs("uk-UK");
+
+        /// <summary>
+        /// Туреций язык.
+        /// </summary>
+        public static Langs Tr = new Langs("tr-TR");
+    }
+}
---
+++ YandexException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace YandexAPI
+{
+    /// <summary>
+    /// Ошибка при запроса на Yandex API.
+    /// </summary>
+    public class YandexException : Exception
+    {
+        /// <summary>
+        /// Ошибка запроса.
+        /// </summary>
+        public Error APIError { get; }
+
+        internal YandexException(Error error) => APIError = error;
+    }
+}
